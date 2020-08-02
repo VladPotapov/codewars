69cadac216ae51bae2cadc7da11da8bd92324467
Roman <kotelnikovrm@gmail.com>
2020-08-02 20:46:05 +0800

    решил задачу YouOnlyNeedOne-Beginner на js

---
+++ 8kyu/YouOnlyNeedOne-Beginner/js/script.js
@@ -0,0 +1,14 @@
+function check(a, x) {
+    for (var i = 0; i < a.length; i++) {
+        if (a[i] == x) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+//codewars
+function check2(a, x) {
+    a.includes(x);   //вернёт true или false;
+}
